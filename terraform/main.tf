95ea1499193efb6006142a139ba717cc63e5dd05
Leon L. Robinson <leon.robinson@qa.com>
2023-03-02 11:53:56 +0000

    add a resource and use it in another resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,6 +38,31 @@ resource "aws_security_group_rule" "leoninstancesecgroupruleSSH" {
   security_group_id = "sg-00a6a30fd569b26bb"
 }
 
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
 resource "aws_instance" "leonsinstance" {
   ami           = var.amiimageid
   instance_type = "t2.micro"
@@ -52,7 +77,7 @@ resource "aws_instance" "leonsinstance2" {
   ami           = var.amiimageid
   instance_type = "t2.small"
   associate_public_ip_address = true
-  vpc_security_group_ids = [var.mysecgroup]
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   key_name = var.mykey
   tags = {
     Name = "LeonInstance2"

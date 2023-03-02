df579c8afb64e13d290cbe06164ded24299df02c
Leon L. Robinson <leon.robinson@qa.com>
2023-03-02 10:06:29 +0000

    my first terraform

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+/*
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+*/
+
+provider "aws" {
+  region  = "eu-west-2"
+}
+
+resource "aws_instance" "demo1" {
+  ami           = "ami-0aaa5410833273cfe"
+  instance_type = "t2.micro"
+
+}

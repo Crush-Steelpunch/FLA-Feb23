8bb1f2c6bdae9afd5a43a82982b708d3892de677
Leon L. Robinson <leon.robinson@qa.com>
2023-03-02 15:57:00 +0000

    rm ref the name

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,7 +54,7 @@ resource "aws_instance" "leonsinstance" {
   ami                         = var.amiimageid
   instance_type               = "t2.micro"
   associate_public_ip_address = true
-  key_name                    = aws_key_pair.terraformkey.name
+  key_name                    = aws_key_pair.terraformkey
   tags = {
     Name = "LeonInstance"
   }
